56b0aca33b5dd4d126b6a94b300164a582e9c876
hakeemulklah <h.jan210@gmail.com>
2019-04-19 23:50:27 +0500

    Updates

---
+++ precache-manifest.0b770aa4412e5e8adc19f8776f581eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-kid-perform/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "697c20d3b62067158279",
+    "url": "/react-kid-perform/static/js/runtime~main.71de6596.js"
+  },
+  {
+    "revision": "dac4af442a3c7cf37ddc",
+    "url": "/react-kid-perform/static/js/main.9b502b7d.chunk.js"
+  },
+  {
+    "revision": "664fcb3371389a6326ed",
+    "url": "/react-kid-perform/static/js/2.25312ee8.chunk.js"
+  },
+  {
+    "revision": "dac4af442a3c7cf37ddc",
+    "url": "/react-kid-perform/static/css/main.91cd4fbe.chunk.css"
+  },
+  {
+    "revision": "67323b53cdf0a75c563ced6e1eacd22f",
+    "url": "/react-kid-perform/index.html"
+  }
+];
